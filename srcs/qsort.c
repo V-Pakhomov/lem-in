01a1eeedee5571d9ceb7cd4d9623acdb39685509
V-Pakhomov <pakhomov.vasiliy.v@gmail.com>
2020-08-21 15:08:25 +0300

    quick sort and binary search was added

---
+++ srcs/qsort.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   qsort.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rciera <rciera@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/21 13:41:51 by rciera            #+#    #+#             */
+/*   Updated: 2020/08/21 14:05:47 by rciera           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void recoursive(char ***rooms, int left, int right, int arr_size)
+{
+	char **m;
+	int l;
+	int r;
+	char **tmp;
+
+	if (left >= right)
+		return;
+	m = (*rooms)[(left + right) / 2];
+	l = left;
+	r = right;
+	while (r > l)
+	{
+		while (ft_strcmp((*rooms)[l], m) < 0)
+			l++;
+		while (ft_strcmp((*rooms)[r], m) > 0)
+			r--;
+		if (r >= l)
+		{
+			tmp = (*rooms)[l];
+			(*rooms)[l] = (*rooms)[r];
+			(*rooms)[r] = tmp;
+			l++;
+			r--;
+		}
+	}
+	recoursive(rooms, left, r, arr_size);
+	recoursive(rooms, l, right, arr_size);
+}
+
+void qsort(char ***rooms, int arr_size)
+{
+	recoursive(rooms, 0, arr_size - 1, arr_size);
+}
