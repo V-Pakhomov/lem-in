50d9717ef7a92eee5e422667198ed45cd1d49301
Mikhail Vysotskiy <comandante3795@gmail.com>
2020-09-06 21:55:55 +0300

    improved error handling, add couple of maps

--- libft/src/ft_isinteger.c
+++ libft/src/ft_isinteger.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isinteger.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rciera <rciera@student.42.fr>              +#+  +:+       +#+        */
+/*   By: admin <admin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/31 13:48:14 by Nik               #+#    #+#             */
-/*   Updated: 2020/09/01 08:50:50 by rciera           ###   ########.fr       */
+/*   Updated: 2020/09/06 16:16:00 by admin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		ft_isinteger(char *nbr)
 {
 	long long num;
 
-	if (!nbr || ft_strlen(nbr) > 11)
+	if (!nbr || ft_strlen(nbr) > 11 || !ft_isnum(nbr))
 		return (0);
 	num = ft_atoll(nbr);
 	return (num <= (long)MAX_INT && num >= (long)MIN_INT);
