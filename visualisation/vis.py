fd05d5c45bda5d913b95aff8795dd704f9a232f4
V-pakhomov <pakhomov.vasiliy.v@gmail.com>
2020-09-21 16:08:28 +0300

    modify visualisation

--- visualisation/vis.py
+++ visualisation/vis.py
@@ -25,11 +25,14 @@ def read_stdin():
 				print('To many vertices')
 				exit(1)
 			room = line.split()[0]
+			rooms_content[room] = []
 			if start:
+				rooms_content[room] = [x for x in range(1, int(ants) + 1)]
+				rooms['start'] = room
 				colors[room] = 'green'
 				node_labels[room] = 'start'
 			elif end:
-				end_room.append(room)
+				rooms['end'] = room
 				node_labels[room] = 'end'
 				colors[room] = 'red'
 			else:
@@ -63,16 +66,36 @@ def is_link(line):
 		return False
 	return len(line.split('-')) == 2
 
+
+def get_point(pt1, pt2, iter):
+	l = iter/(25-iter)
+	x = (pt1[0] + l*pt2[0])/(1 + l)
+	y = (pt1[1] + l*pt2[1])/(1 + l)
+	return [x, y]
+
+def ants_movement(iter, commands):
+	x = []
+	y = []
+	for cmd in commands:
+		pass
+
+
 def print_graph(iter):
 	ax.clear()
 	iter -= 1
 	if iter >= 0 and iter < len(commands):
 		for cmd in commands[iter]:
-			ant = cmd.split('-')[0][1:]
+			ant = int(cmd.split('-')[0][1:])
 			room = cmd.split('-')[1]
-			if room != end_room[0]:
+			old_room = ant_location[ant]
+			rooms_content[old_room].remove(ant)
+			rooms_content[room].append(ant)
+			ant_location[ant] = room
+			if room != rooms['end']:
 				node_labels[room] = str(ant)
 				colors[room] = 'blue'
+	node_labels[rooms['start']] = 'start (' + str(len(rooms_content[rooms['start']])) + ' ants)'
+	node_labels[rooms['end']] = 'end (' + str(len(rooms_content[rooms['end']])) + ' ants)'
 	nx.draw_networkx_nodes(G, pos, node_color=[colors[x] for x in colors.keys()], alpha=0.5, node_size=300)
 	nx.draw_networkx_edges(G, pos, width=0.5, edge_color='b', alpha=0.5)
 	nx.draw_networkx_labels(G, pos, labels=node_labels)
@@ -80,7 +103,7 @@ def print_graph(iter):
 		for cmd in commands[iter]:
 			ant = cmd.split('-')[0][1:]
 			room = cmd.split('-')[1]
-			if room != end_room[0]:
+			if room != rooms['end']:
 				node_labels[room] = ''
 
 
@@ -94,8 +117,10 @@ G = nx.Graph()
 colors = {}
 node_labels = {}
 commands = []
-end_room = []
+rooms = {}
+rooms_content = {}
 read_stdin()
+ant_location = {x:rooms['start'] for x in range(1, int(ants)+1)}
 pos = nx.fruchterman_reingold_layout(G)
 anim = animation.FuncAnimation(fig, print_graph, frames=len(commands)+2, interval=1000, repeat=False)
 plt.show()
